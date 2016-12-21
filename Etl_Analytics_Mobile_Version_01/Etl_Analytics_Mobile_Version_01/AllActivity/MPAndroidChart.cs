256ca96e6f45aa6a15d841ef67fe2f51f6f8a55e
ViborKovacic <vibor.kovacic@gmail.com>
2016-12-21 17:01:01 +0100

    21.12.2016

--- Etl_Analytics_Mobile_Version_01/Etl_Analytics_Mobile_Version_01/AllActivity/MPAndroidChart.cs
+++ Etl_Analytics_Mobile_Version_01/Etl_Analytics_Mobile_Version_01/AllActivity/MPAndroidChart.cs
@@ -1,31 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using MikePhil.Charting.Charts;
 using Etl_Analytics_Mobile_Version_01.Class.Table_Constructor;
-using MikePhil.Charting.Interfaces.Datasets;
 using MikePhil.Charting.Data;
 using Android.Support.V7.App;
-using Java.Util;
 using Etl_Analytics_Mobile_Version_01.Class;
-using MikePhil.Charting.Util;
 using Android.Graphics;
+using MikePhil.Charting.Components;
+using MikePhil.Charting.Formatter;
 
 namespace Etl_Analytics_Mobile_Version_01.AllActivity
 {
     [Activity(Label = "Activity1", Theme = "@style/MyTheme2")]
     public class MPAndroidChart : AppCompatActivity
     {
-        private List<BarEntry> listOfEntrys;
-        private List<string> listOfStrings;
+        private Dictionary<string, List<BarEntry>> dicOfDataSets;
+        private List<LegendEntry> listOfStrings;
+        private List<string> listTableNames;
         private WebService webService;
         private List<StatsTables> listStatsTables;
         private BarDataSet dataSet;
@@ -35,38 +30,65 @@ namespace Etl_Analytics_Mobile_Version_01.AllActivity
 
             // Create your application here
             SetContentView(Resource.Layout.Graph);
-            listOfEntrys = new List<BarEntry>();
-            listOfStrings = new List<string>();
-
+            dicOfDataSets = new Dictionary<string, List<BarEntry>>();
+            listOfStrings = new List<LegendEntry>();
+            listTableNames = new List<string>();
             BarChart chart = FindViewById<BarChart>(Resource.Id.chart);
             webService = new WebService();
             listStatsTables = webService.GetAllDataStatsTable();
 
+
+            foreach (StatsTables row in listStatsTables)
+            {
+                if (!listTableNames.Contains(row.table_name))
+                {
+                    listTableNames.Add(row.table_name);                    
+                }
+            }
             int counter = 0;
 
-            foreach (StatsTables item in listStatsTables)
+            foreach (string table in listTableNames)
             {
-                listOfEntrys.Add(new BarEntry(counter, item.diff_last_trans));
-                counter++;
+                List<BarEntry> barEntry = new List<BarEntry>();
+                foreach (StatsTables item in listStatsTables)
+                {
+                    if (table == item.table_name)
+                    {
+                        barEntry.Add(new BarEntry(counter, item.diff_last_trans));
+                        counter++;
+                    }
+                }
+                dicOfDataSets.Add(table, barEntry);
             }
 
-            listOfStrings.Add("January");
-            listOfStrings.Add("February");
-            listOfStrings.Add("March");
-            listOfStrings.Add("April");
-            listOfStrings.Add("May");
-            listOfStrings.Add("June");
-
-
-            dataSet = new BarDataSet(listOfEntrys, "Statistics");
-            dataSet.SetColor(Color.Black, 100);
-            dataSet.SetStackLabels(listOfStrings.ToArray());
-
             BarData data = new BarData();
-            data.AddDataSet(dataSet);
+
+            string[] test = { "" };
+
+            foreach (KeyValuePair<string, List<BarEntry>> dicDataSet in dicOfDataSets)
+            {
+                dataSet = new BarDataSet(dicDataSet.Value, "");
+                foreach (BarEntry item in dicDataSet.Value)
+                {
+                    if (item.GetY() > 70)
+                    {
+                        dataSet.SetColor(Color.Red, 200);                    
+                    }
+
+                    else
+                    {
+                        dataSet.AddColor(Color.Blue);
+                    }
+                }
+
+                data.AddDataSet(dataSet);
+                //data.SetValueTextColor(Color.CadetBlue);
+                //data.SetValueTextSize(40);
+            }
 
             chart.Data = data;
-            
+            chart.AnimateXY(3000, 3000);
+            chart.SetDrawGridBackground(false);
         }
     }
 }
